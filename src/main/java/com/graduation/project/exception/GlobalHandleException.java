5d1686c92140225ffdc52141be8b8abb3da71a86
Anhhuan2k3 <huan1622k3@gmail.com>
2025-10-13 08:22:11 +0700

    update throw exception when login fails

--- src/main/java/com/graduation/project/exception/GlobalHandleException.java
+++ src/main/java/com/graduation/project/exception/GlobalHandleException.java
@@ -3,6 +3,7 @@ package com.graduation.project.exception;
 import com.graduation.project.dto.response.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -20,4 +21,27 @@ public class GlobalHandleException {
 
     return ResponseEntity.badRequest().body(apiResponse);
   }
+
+    @ExceptionHandler(value = AppException.class)
+    ResponseEntity<ApiResponse<?>> handlingAppException(AppException exception) {
+        ErrorCode errorCode = exception.getErrorCode();
+        ApiResponse<?> apiResponse = new ApiResponse<>();
+
+        apiResponse.setCode(errorCode.getCode());
+        apiResponse.setMessage(errorCode.getMessage());
+
+        return ResponseEntity.status(errorCode.getHttpStatusCode()).body(apiResponse);
+    }
+
+
+    @ExceptionHandler(value = BadCredentialsException.class)
+    ResponseEntity<ApiResponse<?>> handleBadCredentials(BadCredentialsException ex) {
+        log.warn("Bad credentials: {}", ex.getMessage());
+
+        ApiResponse<?> apiResponse = new ApiResponse<>();
+        apiResponse.setCode(ErrorCode.UNAUTHORIZED.getCode());
+        apiResponse.setMessage("Invalid email or password");
+
+        return ResponseEntity.status(ErrorCode.UNAUTHORIZED.getHttpStatusCode()).body(apiResponse);
+    }
 }
