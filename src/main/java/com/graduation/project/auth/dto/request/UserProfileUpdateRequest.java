91c9837d7e3556e4e4c08a4ac356b4b4b5de787c
Zayn Hargreaves <quanva.b21dccn618>
2025-12-24 11:34:17 +0700

    feat(api): update api for update user profile

--- src/main/java/com/graduation/project/auth/dto/request/UserProfileUpdateRequest.java
+++ src/main/java/com/graduation/project/auth/dto/request/UserProfileUpdateRequest.java
@@ -1,9 +1,5 @@
 package com.graduation.project.auth.dto.request;
 
-import com.graduation.project.auth.validation.FileSize;
-import com.graduation.project.auth.validation.FileType;
-import jakarta.annotation.Nullable;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -11,7 +7,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
 
@@ -21,18 +16,13 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserProfileUpdateRequest {
-    @NotBlank(message = "Họ tên không được để trống")
-    String fullName;
 
-    @Nullable
-    @FileSize(max = 5242880, message = "Kích thước file không được vượt quá 5MB")
-    @FileType(allowed = { "image/jpeg", "image/png", "image/jpg" }, message = "Chỉ chấp nhận file ảnh")
-    MultipartFile avatarFile;
+    String fullName;
 
     @Pattern(regexp = "^[0-9]{10}$", message = "Số điện thoại không hợp lệ")
     String phone;
 
     String studentCode;
     String classCode;
-    LocalDate dob;
+
 }
