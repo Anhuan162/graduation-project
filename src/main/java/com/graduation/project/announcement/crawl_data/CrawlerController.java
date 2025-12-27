11968c2f889e9bc5639d871e9beae94a5a2acb42
Anhhuan2k3 <huan1622k3@gmail.com>
2025-12-27 16:51:33 +0700

    update

--- src/main/java/com/graduation/project/announcement/crawl_data/CrawlerController.java
+++ src/main/java/com/graduation/project/announcement/crawl_data/CrawlerController.java
@@ -1,7 +1,7 @@
 package com.graduation.project.announcement.crawl_data;
 
+import com.graduation.project.auth.dto.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +14,9 @@ public class CrawlerController {
   private final CrawlerService crawlerService;
 
   @PostMapping("/trigger")
-  public ResponseEntity<String> triggerCrawl() {
+  public ApiResponse<String> triggerCrawl() {
     // Nên chạy async để không block thread của API
     new Thread(crawlerService::crawlAll).start();
-    return ResponseEntity.ok("Đã kích hoạt tiến trình Crawl dữ liệu!");
+    return ApiResponse.<String>builder().result("Đã kích hoạt tiến trình Crawl dữ liệu!").build();
   }
 }
